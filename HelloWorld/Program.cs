b69fdb373d2adcdfad52b27df758a720cd4eab05
Kyle Jarman <Kyle Jarman>
2017-11-16 15:31:16 +1100

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var memes = "HELLO";
+            Console.WriteLine("Henlo World");
+            Console.WriteLine(memes);
+            Console.ReadLine();
+        }
+    }
+}
